cd94dffcd258a097358b41386acc3407cc8ee30d
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-05-14 14:33:13 +0200

    blah

---
+++ akatpp/akat_static_var.py
@@ -0,0 +1,79 @@
+import akat_write_cflags
+
+defined_static_vars = []
+configured_as_reg = {}
+used_reg = {}
+next_reg_n = 16
+
+def configure_as_reg(v):
+    """Call me and I will configure your variable 'v' to be a hardware register."""
+
+    global next_reg_n
+
+    reg = "r" + str(next_reg_n)
+    next_reg_n += 1
+    configured_as_reg[v] = reg
+
+    akat_write_cflags.add("-ffixed-" + reg)
+
+    return reg
+
+# =================================================================================
+# Implementation of macro
+
+class Macro:
+    def render(self, inv):
+        ctx = akat.prepare(inv, required_args = ["decl"], required_enclosing_macros = ["GLOBAL", "SCOPE"], keywords = ["ignore_dups"])
+
+        t, name = ctx.decl.rsplit(" ", 1)
+
+        ns = akat.get_current_namespace()
+        full_name = ns + "__" + name
+
+        # Avoid duplicated definitions + we do some summary at the end
+        already_defined = full_name in defined_static_vars
+
+        if already_defined:
+            if not ignore_dups:
+                akat.fatal_error(STRESS(name), " is already defined (", ns, ")")
+        else:
+            defined_static_vars.append(full_name)
+
+            if full_name in configured_as_reg:
+                reg = configured_as_reg[full_name]
+                used_reg[full_name] = reg
+                mod = "register"
+                attr = " asm (\"" + reg + "\")"
+            else:
+                mod = "static"
+                attr = ""
+
+            ctx.GLOBAL.add_global_code(mod +" " + t + " "  + full_name + attr + ";")
+
+        ctx.SCOPE.map_as(full_name, name, ignore_dups = ctx.ignore_dups)
+
+        return "";
+
+    def postvalidate():
+        akat.print_sep()
+        akat.print_info("Defined static vars:")
+        for v in defined_static_vars:
+            extra = ""
+            if v in used_reg:
+                extra = used_reg[v]
+
+            akat.print_info("    ", STRESS_COLOR, v, "   ", RESET_COLOR, extra)
+
+        # Unused regs
+        shown_unused_header = False
+        for v in configured_as_reg:
+            if v not in used_reg:
+                if not shown_unused_header:
+                    akat.print_sep()
+                    akat.print_error("Following static variables are configured to use registers. But we can't find such static variables!")
+                    shown_unused_header = True
+
+                akat.print_error("    ", STRESS_COLOR, v)
+
+        if shown_unused_header:
+            akat.fatal_error("Please fix it!")
---
+++ benchmark/static_var.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
+
+#include "benchmark.h"
+
+WRITE_CFLAGS$(build/static_var);
+
+GLOBAL$() {
+    static void hello() {
+        SCOPE$(s1) {
+            STATIC_VAR$(u8 x);
+
+            x = 3;
+
+            SCOPE$(s2, restore) {
+                STATIC_VAR$(u8 y);
+
+                y = 4;
+
+                if (x == 3 && y == 4) {
+                    BENCH;
+                    BENCH_EXIT;
+                }
+            }
+        }
+    }
+}
+
+// Main
+X_MAIN$(cpu_freq = 8000000) {
+    BENCH_INIT;
+    hello();
+}
