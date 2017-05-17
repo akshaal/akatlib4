3be5de2c416952830183ed32152d0a3caf511d12
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-05-17 16:01:35 +0200

    more stuff

--- benchmark/clock_w_kbd_tm1637_f1.c
+++ benchmark/clock_w_kbd_tm1637_f1.c
@@ -7,15 +7,14 @@
 WRITE_CFLAGS$(build/clock_w_kbd_tm1637_f1);
 
 USE_REG$(tm1637__dirty);
-USE_REG$(akat_clock_current_decisecond);
-USE_REG$(akat_clock_current_second_l);
-//USE_REG$(akat_clock_current_second_h);
-//USE_REG$(akat_clock_current_minute_l);
-//USE_REG$(akat_clock_current_minute_h);
-//USE_REG$(akat_clock_current_hour_h, low);
-//USE_REG$(akat_clock_current_hour_l, low);
+USE_REG$(akat_clock_started__c1);
+USE_REG$(akat_timestamp_decisecond__t);
 
 X_TM1637$(tm1637, clk = B3, dio = B4);
+
+X_TIMESTAMP$(t);
+
+X_CLOCK$(c1, t);
 
 GLOBAL$() {
     STATIC_VAR$(u32 sum);
@@ -26,28 +25,28 @@ X_BUTTON$(button2, D1) { sum += 200000L; }
 X_BUTTON$(button3, D2) { sum += 400000L; }
 X_BUTTON$(button4, D3) { sum += 800000L; }
 
-X_CLOCK$(tm1637_clock) {
+X_TIMESTAMP_CALLBACKS$(t) {
     METHOD$(void on_new_minute_h(), inline) {
-        tm1637.set_digit_pos_1(tm1637_clock.get_current_minute_h(), 0);
+        tm1637.set_digit_pos_1(t.get_minutes_h(), 0);
     }
 
     METHOD$(void on_new_minute_l(), inline) {
-        tm1637.set_digit_pos_2(tm1637_clock.get_current_minute_l(), sum == 150000);
+        tm1637.set_digit_pos_2(t.get_minutes_l(), sum == 150000);
     }
 
     METHOD$(void on_new_second_h(), inline) {
-        tm1637.set_digit_pos_3(tm1637_clock.get_current_second_h(), 0);
+        tm1637.set_digit_pos_3(t.get_seconds_h(), 0);
     }
 
     METHOD$(void on_new_second_l(), inline) {
-        tm1637.set_digit_pos_4(tm1637_clock.get_current_second_l(), 0);
+        tm1637.set_digit_pos_4(t.get_seconds_l(), 0);
     }
 }
 
-X_CLOCK$(c1) {
+X_TIMESTAMP_CALLBACKS$(t) {
     METHOD$(void on_new_decisecond(), inline) {
         BENCH;
-        if (c1.get_current_second_l() == 3 && c1.get_current_decisecond() == 5) {
+        if (t.get_seconds_l() == 3 && t.get_deciseconds() == 5) {
             BENCH_EXIT;
         }
     }
@@ -60,5 +59,6 @@ X_CLOCK$(c1) {
 // Main
 X_MAIN$(cpu_freq = 1000000) {
     BENCH_INIT;
+    c1.start();
     sei();
 }
