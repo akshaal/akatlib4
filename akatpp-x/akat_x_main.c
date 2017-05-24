e087bcb66845b28d4029560f2917561609e7bf97
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-05-25 00:52:34 +0200

    Revert "Revert "more stuff""
    This reverts commit b8f6f805bc0969fccf79298654d0fb55a0999411.

--- akatpp-x/akat_x_main.c
+++ akatpp-x/akat_x_main.c
@@ -1,4 +1,8 @@
 FUNCTION$(AKAT_NO_RETURN void main(), not_static) {
+    // TODO: INIT ALL FIXED REGS TO ZERO!!!!!!!
+
+    asm volatile("inc r3" : "=r" (__akat_one__));
+
     % for init in inits:
         ${init}();
     % endfor
