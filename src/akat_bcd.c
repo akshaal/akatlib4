fc4a92b1d6e2d1e672e780c31000cbcd3df85267
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-05-26 20:32:05 +0200

    blah

---
+++ src/akat_bcd.c
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////////////////////
+// Useful functions for rapid development for AVR microcontrollers.
+// 2017 (C) Akshaal, Apache License
+///////////////////////////////////////////////////////////////////
+
+static AKAT_UNUSED AKAT_CONST AKAT_PURE u8 akat_bcd_inc(u8 bcd) {
+    if (AKAT_BCD_GET_L(bcd) == 9) {
+        bcd += 16 - 9;
+    } else {
+        bcd++;
+    }
+
+    return bcd;
+}
+
+static AKAT_UNUSED AKAT_CONST AKAT_PURE u8 akat_bcd_dec(u8 bcd) {
+    if (bcd & 15) {
+        bcd -= 16 - 9;
+    } else {
+        bcd--;
+    }
+
+    return bcd;
+}
