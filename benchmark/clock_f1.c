866af2f401df8913dbb4f202a0deab2c3d74b683
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-05-26 20:43:54 +0200

    blah

--- benchmark/clock_f1.c
+++ benchmark/clock_f1.c
@@ -9,9 +9,9 @@ X_CPU$(cpu_freq = 1000000);
 
 USE_REG$(akat_clock_started__c1);
 USE_REG$(akat_timestamp_decisecond__t);
-USE_REG$(akat_timestamp_second_h__t);
-USE_REG$(akat_timestamp_second_l__t);
-USE_REG$(akat_timestamp_minute_h__t);
+USE_REG$(akat_timestamp_second__t);
+USE_REG$(akat_timestamp_hour__t);
+USE_REG$(akat_timestamp_minute__t);
 
 X_TIMESTAMP$(t);
 
@@ -25,15 +25,15 @@ X_TIMESTAMP_CALLBACKS$(t) {
         }
     }
 
-    METHOD$(void on_new_second_l(), inline) {
+    METHOD$(void on_new_second(), inline) {
         BENCH;
     }
 
-    METHOD$(void on_new_minute_l(), inline) {
+    METHOD$(void on_new_minute(), inline) {
         BENCH;
     }
 
-    METHOD$(void on_new_hour_l(), inline) {
+    METHOD$(void on_new_hour(), inline) {
         BENCH;
     }
 }
