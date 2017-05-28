c4b7f79ac9de52f70a08bf699a7822e74cad4f8d
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-05-28 21:46:19 +0200

    blah

--- akatpp-x/akat_x_button_full.c
+++ akatpp-x/akat_x_button_full.c
@@ -16,10 +16,10 @@ OBJECT$(${object_name}) {
 }
 
 RUNNABLE$(${object_name}__runnable) {
-    akat_x_button_handle_pin_state(
-        &${object_name}__state,
-        ${object_name}__input.is_set(),
-        ${object_name}.on_press,
-        ${object_name}.on_release
-    );
+    akat_x_button_action_t const rc = akat_x_button_handle_pin_state(&${object_name}__state, ${object_name}__input.is_set());
+    if (rc == AKAT_X_BUTTON_ACTION_KEYPRESS) {
+        ${object_name}.on_press();
+    } else if (rc == AKAT_X_BUTTON_ACTION_KEYRELEASE) {
+        ${object_name}.on_release();
+    }
 }
