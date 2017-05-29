e967b9507a472f581810e465612665235dbf42ff
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-05-29 14:45:57 +0200

    blah

--- src/akat_bcd.h
+++ src/akat_bcd.h
@@ -6,6 +6,3 @@
 #define AKAT_BCD_GET_L(x)     ((x) & 15)
 #define AKAT_BCD_GET_H(x)     (((x) / 16))
 #define AKAT_BCD(h, l)        (((h) * 16) + (l))
-
-static AKAT_UNUSED AKAT_CONST AKAT_PURE u8 akat_bcd_inc(u8 bcd);
-static AKAT_UNUSED AKAT_CONST AKAT_PURE u8 akat_bcd_dec(u8 bcd);
